9fe6d483f3c17453e6077f10ed049b61656b0edf
mrWheel <Willem@Aandewiel.nl>
2017-11-14 14:08:14 +0100

    Update Cli_Example1.ino
    Registered function names are case insensitive
    You can use param's in functions

--- examples/Cli_Example1/Cli_Example1.ino
+++ examples/Cli_Example1/Cli_Example1.ino
@@ -2,20 +2,34 @@
 
 Cli cli = Cli(Serial);
 
-void FunctionHello(void) {Serial.println("\"functionHello\" is being executed!");}
+void FunctionHello(void) {
+  Serial.println("\"functionHello\" is being executed!");
+  Serial.print("FunctionHello() parm: ["); 
+  Serial.print(cli.parm); // <== you can do something usefull with the String "cli.parm"  
+  Serial.println("]");
+}
+
 void FunctionByebye(void) {Serial.println("\"functionByebye\" is being executed!");}
 void FunctionSeeyou(void) {Serial.println("\"functionSeeyou\" is being executed!");}
+void FunctionHelp(void) {Serial.println("==Help==\nType:\nHello\nByebye\nSeeyou\n");}
 
 void setup(){
-  cli.RegisterCmd("hello",&FunctionHello);
-  cli.RegisterCmd("byebye",&FunctionByebye);
-  cli.RegisterCmd("seeyou",&FunctionSeeyou);
-  Serial.begin(115200);
+  cli.RegisterCmd("help",&FunctionHelp);
+  cli.RegisterCmd("Hello",&FunctionHello); // <== case does not matter
+  cli.RegisterCmd("ByeBye",&FunctionByebye);
+  cli.RegisterCmd("seeYou",&FunctionSeeyou);
+  Serial.begin(19200);
+
+  Serial.println("Enter command or type 'help'");
 }
 
 void loop(){
   cli.Run();
+    
 }  
+
+
+
 
 
 
